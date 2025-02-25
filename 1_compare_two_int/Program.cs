45e1d452352fe8519da5d4405a14cbe2715a8ee8
Avenir Baryshev <avenirpik@gmail.com>
2025-02-25 16:15:26 +0700

    1 rdy

--- 1_compare_two_int/Program.cs
+++ 1_compare_two_int/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace _1_compare_two_int
+﻿namespace _1_compare_two_double
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Вводятся два числа. Сравнить их и вывести правильный знак «<», «>» или «=».\n");
+            Console.WriteLine("Введите первое число");
+            double dig1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double dig2 = Convert.ToDouble(Console.ReadLine());
+
+            if (dig1 == dig2)
+            {
+                Console.WriteLine("{0} = {1}", dig1, dig2);
+            }
+            if (dig1 > dig2)
+            {
+                Console.WriteLine("{0} > {1}", dig1, dig2);
+            }
+            if (dig1 < dig2)
+            {
+                Console.WriteLine("{0} < {1}", dig1, dig2);
+            }
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
         }
     }
 }
