d992ee6117dad9d6ea030f756c59d50af9645c5a
Avenir Baryshev <avenirpik@gmail.com>
2025-02-25 16:25:38 +0700

    2 rdy

---
+++ 2_bigger_of_three/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _2_bigger_of_three
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводятся три числа. Вывести большее их них\n не используя Math, на сколько я понял");
+            Console.WriteLine("Введите первое число");
+            double dig1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double dig2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третье число");
+            double dig3 = Convert.ToDouble(Console.ReadLine());
+            double maxdig = dig1;
+
+            if (dig2 > maxdig) maxdig = dig2;
+            if (dig3 > maxdig) maxdig = dig3;
+
+            Console.WriteLine("\n{0} - наибольшее число\n", maxdig);
+
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
