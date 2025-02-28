cfa72e569ffe61f586b9f3b029277af56b50b6db
Avenir Baryshev <avenirpik@gmail.com>
2025-02-28 10:53:22 +0700

    сделано 8

---
+++ 8_from_-10_to_10/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _8_from__10_to_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводится число. Вывести «Да», если оно попадает в диапазон [-10,10], и «Нет» в противном случае.");
+            Console.WriteLine("Введите число\n");
+
+            int dig1 = Convert.ToInt32(Console.ReadLine());
+
+            if (dig1 >= -10 && dig1 <= 10) Console.WriteLine("\nДа");
+            else Console.WriteLine("\nНет");
+
+            Console.WriteLine("\nPress any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
